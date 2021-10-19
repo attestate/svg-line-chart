b64a0eb944f5af52ec8b470f0c77aae901014660
soulofmischief <hello@badsoft.co>
2021-10-19 17:10:52 -0500

    Clarify code

--- src/index.mjs
+++ src/index.mjs
@@ -268,7 +268,9 @@ export function scaleDates(
     const distanceFromStart = differenceInDays(firstDayOfMonth, start);
 
     const
+      // Get formatted date.
       _name = format(firstDayOfMonth, "MMM yy"),
+      // Abbreviate date and add an apostrophe.
       name = [_name.slice( 0, -2 ), "'", _name.slice( -2 )].join('')
 
     return {
