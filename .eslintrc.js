928f646f31df0b182cd8999e8edc79568c59fa4f
depth <depth@localhost.localdomain>
2021-11-09 19:05:53 +0530

    Setting up eslint configuration file

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 13,
+        "sourceType": "module"
+    },
+    "rules": {
+      "camelcase": "error",
+      "no-unused-vars": "warn"
+    }
+};
