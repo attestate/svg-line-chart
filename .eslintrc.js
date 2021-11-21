5933c121bba2d5e29298832e6b333c5651cd0cec
Minh Pham <est2000vn@gmail.com>
2021-11-21 17:17:21 -0500

    ran prettier & added integration with eslint
    - Ran prettier through the entire project
    - Installed eslint-config-prettier and eslint-plugin-prettier as dev dependencies
    - Updated .eslintrc.js to include prettier intergration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,18 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 13,
-        "sourceType": "module"
-    },
-    "rules": {
-      "camelcase": "error",
-      "no-unused-vars": "warn"
-    }
-};
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  plugins: ['prettier'],
+  rules: {
+    camelcase: 'error',
+    'no-unused-vars': 'warn',
+    'prettier/prettier': ['error'],
+  },
+}
